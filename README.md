9df9ac9b11eb53698416da9a28e406df1a8c8cc1
c0d-0x <c0d_0x007@proton.me>
2023-12-05 00:57:12 +0100

    Migrated to nvchad - custom-convigs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Configuration files
-+ nvim config with vim-plug
+
+- nvim config with vim-plug (This was my old Configuration)
+
+- I Realized my vim-plug config was running slow because of coc language server. I tried **NvChad** and I was really amazed by its features and simplicity.
---
+++ custom-for-nvchad/configs/lspconfig.lua
@@ -0,0 +1,53 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+local lspconfig = require "lspconfig"
+local prettier = require("prettier")
+
+lspconfig.cssls.setup({
+ on_attach = on_attach,
+ capabilities = capabilities,
+ filetype = { "css", "scss", "less" }
+})
+
+lspconfig.pyright.setup({
+ on_attach = on_attach,
+ capabilities = capabilities,
+ filetype = { "python" }
+})
+
+lspconfig.clangd.setup({
+  on_attach = function (client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+  filetype = {"c", "cpp"},
+})
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities =  capabilities,
+  filetype = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "html" },
+    embeddedLanguages = {
+    html = true
+  },
+  provideFormatter = true
+})
+
+lspconfig.html.setup({
+  on_attach = function ()
+    capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+  end,
+  capabilities = capabilities,
+  filetype = { "html", "css", "javascript" },
+  embeddedLanguages = {
+    css = true,
+    javascript = true,
+  },
+  provideFormatter = true,
+
+})
+
+prettier.setup(require("custom.configs.prettierconfig"))
